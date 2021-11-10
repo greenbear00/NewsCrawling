338416513dc8abe6e90ec2b8e1940634aec46c5c
greenbear00 <greenbear.woo@gmail.com>
2021-11-10 16:56:13 +0900

    feat: 전체 언론사를 검색 및 특정 언론사에 대한 언론사별 랭킹 뉴스 확인 기능 추가

--- jtbc_crawler/parser/naver_ranking_news.py
+++ jtbc_crawler/parser/naver_ranking_news.py
@@ -10,12 +10,47 @@ class NaverRankingNews(ChromeDriver, metaclass=Singleton):
         print(f"naver ranking news url = {url}")
         self.browser.get(url)
 
-        self._load_ranking_news()
+        self._publisher = {}
+        self._click_more_publisher()
+        self._find_publisher()
+        self._go_publisher(publisher_name="JTBC")
 
-    def _load_ranking_news(self):
-        rank_box = self.browser.find_elements(By.CLASS_NAME, "rankingnews_box")
-        for a_box in rank_box:
-            print(a_box)
+    # def _load_ranking_news(self):
+    #     rank_box = self.browser.find_elements(By.CLASS_NAME, "rankingnews_box")
+    #     for a_box in rank_box:
+    #         print(a_box)
+
+    @property
+    def publisher(self):
+        return self._publisher
+
+    def _find_publisher(self):
+        news_name = self.browser.find_elements(By.CLASS_NAME, "rankingnews_name")
+        self._publisher = {news.text:news for news in news_name}
+
+
+    def _go_publisher(self, publisher_name:str):
+        jtbc_publisher = self.publisher.get(publisher_name)
+        jtbc_publisher.click()
+
+
+
+    def _click_more_publisher(self):
+        # 아래 button에 대한 xpath
+        # <button type="button" class="button_rankingnews_more _moreButton nclicks('RBP.more')">다른 언론사 랭킹 더보기</button>
+        # xpath: /html/body/div/div[4]/button
+        # WebElement(button)
+        while True:
+            button = self.browser.find_element(By.XPATH, '/html/body/div/div[4]/button')
+            if button.aria_role == "button" and button.tag_name == "button":
+                button.click()
+            else:
+                break
+
+
+
+        print("done")
+
 
 
 if __name__ == "__main__":
